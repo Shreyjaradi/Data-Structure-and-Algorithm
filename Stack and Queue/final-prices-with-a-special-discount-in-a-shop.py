5fa3145e7586894ef62d54225711661acb51da91
Shrey Jaradi <shreyjaradi@outlook.com>
2022-06-13 23:32:24 +0530

    Create final-prices-with-a-special-discount-in-a-shop.py

---
+++ Stack and Queue/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+            p =0
+            q = 1
+            while(q <len(prices)):
+                if(prices[q] <= prices[p]):
+                        prices[p] = prices[p] - prices[q]
+                        p = p + 1
+                        q = p + 1
+                else:
+                    if (q == len(prices)-1 and prices[q]>prices[p]):
+                        prices[p] = prices[p]
+                        p = p +1
+                        q = p+1
+                    else:
+                        q = q +1            
+            return prices       
