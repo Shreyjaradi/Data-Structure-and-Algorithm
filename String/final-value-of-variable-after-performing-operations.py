fa374d5e23fe6f016c3cc1c7ae86725c3841c54f
Shrey Jaradi <shreyjaradi@outlook.com>
2022-06-14 13:17:16 +0530

    Create final-value-of-variable-after-performing-operations.py

---
+++ String/final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,13 @@
+#Link to problem : https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x=0
+        for i in operations :
+            if(i== '++X' or i== 'X++'):
+                x = x + 1    
+            elif (i=='--X' or i=='X--'):
+                x = x - 1
+        print(x)
+        return x
+        
