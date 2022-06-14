11991f0b510ecf5031e1184f83903231b72d7015
Shrey Jaradi <shreyjaradi@outlook.com>
2022-06-14 13:22:08 +0530

    Create concatenation-of-array.py

---
+++ Array/concatenation-of-array.py
@@ -0,0 +1,6 @@
+# link to problem : https://leetcode.com/problems/concatenation-of-array/
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = nums + nums   
+        return ans
