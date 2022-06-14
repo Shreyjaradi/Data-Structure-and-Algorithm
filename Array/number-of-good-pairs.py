3715614c90a24a573a4f3bfbb07db158204af388
Shrey Jaradi <shreyjaradi@outlook.com>
2022-06-14 13:24:57 +0530

    Create number-of-good-pairs.py

---
+++ Array/number-of-good-pairs.py
@@ -0,0 +1,9 @@
+# link to problem : https://leetcode.com/problems/number-of-good-pairs/
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        for i in nums.sort():
+            print(i)
+               
+        return count
+        
